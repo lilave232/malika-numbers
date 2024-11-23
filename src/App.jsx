cd2766afe39155ee600c1528834a4fca57218dea
Avery Pozzobon <averypozzobon@gmail.com>
2024-11-23 14:50:44 -0500

    App

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import './App.css'
 function App() {
   const words = ["Plein","Carre","Cheval","Transversale","Sixain"];
   const numbers = [2,3,4,5,6,7,8,9,10];
-  const words2 = ["Plein","Cheval"];
+  const words2 = ["Plein","Carre","Cheval","Transversale","Sixain"];
   const numbers2 = [11,12,13,14,15,16,17,18,19,20];
   //const words = ["Cheval"];
   //const numbers = [11,12,13,14,15,16,17,18,19,20];
